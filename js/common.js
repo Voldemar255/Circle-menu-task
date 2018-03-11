68050185201e95131d9ef06b6d2fed826369d033
Volodymyr Vinnichuk <vinnichuk.vladimir@gmail.com>
2018-03-11 13:32:15 +0200

    some changes

--- js/common.js
+++ js/common.js
@@ -10,3 +10,21 @@ for (var i = 0; i < divs.length; i++) {
 
     angle += delta;
 }
+
+var text = document.getElementsByClassName('text');
+var delta = Math.PI * 2 / text.length;
+var x = 0, y = 0, angle = 0;
+
+for (var i = 0; i < text.length; i++) {
+    text[i].style.position = 'absolute';
+    text[i].style.left =  280 * Math.cos(angle) + 'px';
+    text[i].style.top = 280 * Math.sin(angle) + 'px';
+
+    angle += delta;
+}
+
+$(function () {
+    $('.outer-circle').on('scroll'), function (event) {
+        (event.target).addClass('visible');
+    }
+});
