1173efc5494bf045b294e9d43494dda52dce046d
Volodymyr Vinnichuk <vinnichuk.vladimir@gmail.com>
2018-03-14 20:24:06 +0200

    scroll position

--- js/common.js
+++ js/common.js
@@ -5,21 +5,21 @@ function radiusPosition(radius, el, angle) {
     var delta = (Math.PI * 2 / elements.length);
     for (var i = 0; i < elements.length; i++) {
         elements[i].style.position = 'absolute';
-        elements[i].style.left = radius * Math.cos(angle) + 'px';
-        elements[i].style.top = radius * Math.sin(angle) + 'px';
+        elements[i].style.left = 10 + radius * Math.cos(angle) + 'px';
+        elements[i].style.top = 325 + radius * Math.sin(angle) + 'px';
         angle += delta;
     }
 }
-radiusPosition(280, "outer-circle", 0);
-radiusPosition(330, "text", 0);
+radiusPosition(290, "outer-circle", 0);
+radiusPosition(360, "text", 0);
 
 window.onwheel = function() {
     var angle = 0;
     var scrolled = window.pageYOffset || window.pageXOffset || document.documentElement.scrollTop;
-    console.log(scrolled);
+    // console.log(scrolled);
     angle += scrolled;
-    radiusPosition(280, "outer-circle", angle);
-    radiusPosition(330, "text", angle);
+    radiusPosition(290, "outer-circle", angle);
+    radiusPosition(360, "text", angle);
 };
 
 // $(function () {
