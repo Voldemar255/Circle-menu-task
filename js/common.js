0a06443ae13766b36f08c9091635cf9eba3fbbf6
Volodymyr Vinnichuk <vinnichuk.vladimir@gmail.com>
2018-03-17 14:34:04 +0200

    fix scroll, add up and down scroll-button

--- js/common.js
+++ js/common.js
@@ -8,30 +8,57 @@ function radiusPosition(radius, el, angle) {
         elements[i].style.left = 10 + radius * Math.cos(angle) + 'px';
         elements[i].style.top = 325 + radius * Math.sin(angle) + 'px';
         angle += delta;
+        // console.log(delta);
     }
 }
 radiusPosition(290, "outer-circle", 0);
 radiusPosition(360, "text", 0);
 
-window.onwheel = function() {
+var elem = window;
+
+if (elem.addEventListener) {
+    if ('onwheel' in document) {
+        // IE9+, FF17+
+        elem.addEventListener("wheel", onWheel);
+    } else if ('onmousewheel' in document) {
+        // устаревший вариант события
+        elem.addEventListener("mousewheel", onWheel);
+    } else {
+        // Firefox < 17
+        elem.addEventListener("MozMousePixelScroll", onWheel);
+    }
+} else { // IE8-
+    elem.attachEvent("onmousewheel", onWheel);
+}
+
+function onWheel(e) {
+    e = e || window.event;
+    var delta = e.deltaY || e.detail || e.wheelDelta;
     var angle = 0;
-    var scrolled = window.pageYOffset || window.pageXOffset || document.documentElement.scrollTop;
-    // console.log(scrolled);
-    angle += scrolled;
+    angle += delta;
+    console.log (angle);
     radiusPosition(290, "outer-circle", angle);
     radiusPosition(360, "text", angle);
+    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
 };
 
-
-var myBtn = document.getElementById('next');
+var myBtnUp = document.getElementById('next');
 var angle = 0;
-myBtn.addEventListener('click', function () {
+myBtnUp.addEventListener('click', function () {
+    var delta = 0.5711986642890533;
+    angle -= delta;
+    radiusPosition(290, "outer-circle", angle);
+    radiusPosition(360, "text", angle);
+});
+
+var myBtnDown = document.getElementById('prev');
+var angle = 0;
+myBtnDown.addEventListener('click', function () {
     var delta = 0.5711986642890533;
     angle += delta;
     radiusPosition(290, "outer-circle", angle);
     radiusPosition(360, "text", angle);
 });
-
 
 
 
